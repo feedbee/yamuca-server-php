06bca6c1ded2b45b3b65916bf4bffe0921523fa8
Valera Leontyev <feedbee@gmail.com>
2015-03-01 13:49:50 +0300

    Additional comments in config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -6,7 +6,9 @@
 
 return array(
     'listen' => [
-        'port' => 8910,
-        'interface' => 'localhost',
+        'port' => 8910,             // 31111 is default port non-standard, use 80 to avoid conflicts with corporate
+                                    // firewalls, or proxy traffic from 80 to 31111 using nginx or haproxy
+        'interface' => 'localhost', // localhost for local connections only (development), 0.0.0.0 for all interfaces,
+                                    // or any concrete address if one of your network interfaces (i.e. 192.168.0.20)
     ],
 );
